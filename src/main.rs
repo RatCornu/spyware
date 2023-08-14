8bb92d270fc80e22e8d15b1ab6058d8ff34378cd
Rat Cornu <balthazar.patiachvili@crans.org>
2023-08-15 01:57:17 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::{collections::HashSet, env, sync::Arc};
+
+use anyhow::Result;
+use log::{error, info, Level};
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{standard::macros::group, StandardFramework},
+    http::Http,
+    model::prelude::{Ready, ResumedEvent},
+    prelude::{Context, EventHandler, GatewayIntents, TypeMapKey},
+    Client,
+};
+use tokio::sync::Mutex;
+
+#[group]
+struct Everyone;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} rejoint la partie !", ready.user.name);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Prêt de nouveau !")
+    }
+}
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    simple_logger::init_with_level(Level::Warn)?;
+    dotenv::dotenv()?;
+
+    let token = env::var("DISCORD_TOKEN")?;
+
+    let http = Http::new(&token);
+    let (owner, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => (info.owner.id, info.id),
+        Err(err) => panic!("Could not access application info: {:?}", err),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.owners(HashSet::from_iter([owner])).prefix("/").allow_dm(true))
+        .group(&EVERYONE_GROUP);
+
+    let intents = GatewayIntents::all();
+
+    let mut client = Client::builder(token, intents).framework(framework).event_handler(Handler).await?;
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(err) = client.start().await {
+        error!("Client error: {:?}", err);
+    }
+
+    Ok(())
+}
